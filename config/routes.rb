c324d586ed63926fd5925056f48ef236f4fd24f0
dialite <raymondolisa01@gmail.com>
2023-03-14 23:04:55 +0100

    Generate rails devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :foods
-  resources :recipes
-  resources :recipefoods
-  resources :users
+  devise_for :users
+  # resources :foods
+  # resources :recipes
+  # resources :recipefoods
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
