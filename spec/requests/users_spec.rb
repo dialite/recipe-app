3a85797ce21cc04caff783067012fc279fcaad30
dialite <raymondolisa01@gmail.com>
2023-03-19 04:44:10 +0100

    Fix users spec simulation for request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,31 +1,53 @@
 require 'rails_helper'
 
-RSpec.feature 'Users', type: :request do
-  before(:each) do
-    @user = User.new(name: 'tamana', email: 'tamana.balkhi1377@gmail.com', password: '123456',
-                     password_confirmation: '123456')
-  end
+RSpec.describe 'Log in page', type: :system do
+  describe 'users/signin' do
+    before(:each) do
+      @user = User.create!(name: 'user', email: 'user@example.com', password: 'password')
+      @user.skip_confirmation!
+      @user.save!
+      visit new_user_session_path
+    end
 
-  it 'is valid with a name' do
-    expect(@user.name).to eq('tamana')
-  end
+    after(:each) do
+      sleep(2)
+    end
 
-  it 'is not valid without a name' do
-    @user.name = nil
-    expect(@user).to_not be_valid
-  end
+    it 'displays email and password input field and submit button' do
+      expect(page).to have_field('Email')
+      expect(page).to have_field('Password')
+      expect(page).to have_button('Log in')
+    end
 
-  it 'is not valid an invalid email' do
-    @user.email = "#{'a' * 244}.com"
-    expect(@user).to_not be_valid
-  end
+    it 'displays detail error with empty credentials' do
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      click_button 'Log in'
+      expect(page).to have_content('Log in')
+    end
 
-  it 'is not valid without a password less than 6 characters' do
-    @user.password = 'a' * 4
-    expect(@user).to_not be_valid
-  end
+    it 'displays error message with invalid credentials' do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'wrong_password'
+      click_button 'Log in'
+      expect(page).to have_content('Sign up')
+    end
 
-  it 'is valid with a name, an email, and a password' do
-    expect(@user).to be_valid
+    it 'Successfully logs in with correct credentials' do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    it 'click on `Forgot your password?`' do
+      click_link 'Forgot your password?'
+      expect(page).to have_current_path new_user_password_path
+    end
+
+    it 'click on `Didn`t receive confirmation instructions?`' do
+      click_link "Didn't receive confirmation instructions?"
+      expect(page).to have_current_path new_user_confirmation_path
+    end
   end
 end
