e8117f78647fc43df029723cbc9261adf1a086ab
dialite <raymondolisa01@gmail.com>
2023-03-19 04:16:06 +0100

    Fix n+1 problem

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,6 +1,6 @@
 class RecipeFoodsController < ApplicationController
   def index
-    @foods = current_user.recipe_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
+    @foods = current_user.recipe_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity).includes(:food)
     @sum = 0
     @foods.each do |food|
       @sum += food.quantity * food.food.price
